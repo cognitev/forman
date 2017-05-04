0736f3e3044f57d41b8a6b3d4cc0e07e28ade278
AlSayed Gamal <mail.gamal@gmail.com>
2017-05-04 15:54:58 +0200

    moved forman in separate folder

--- README.rst
+++ README.rst
@@ -1,19 +1,27 @@
-##Install
-pip install forman
-in urls.py
-url(r'^forman/', include('forman.urls',namespace='forman')),
-in settings.py
-INSTALLED_APPS = [
-...
-'forman',
-...
-]
+=====
+forman
+=====
 
+forman is a simple Django app to create survery forms and collect and export submissions
 
-##usage
-you can link forman to any model
+Quick start
+-----------
 
-from forman import *
+1. Add "forman" to your INSTALLED_APPS setting like this::
 
-class poll(models.Model):
-    survey = models.ForeignKey(Survey)
+    INSTALLED_APPS = [
+        ...
+        'forman',
+    ]
+
+2. Include the polls URLconf in your project urls.py like this::
+
+    url(r'^forman/', include('forman.urls',namespace='forman')),
+
+3. Run `python manage.py migrate` to create the forman models.
+
+4. Start the development server and visit http://127.0.0.1:8000/admin/
+   to create a survey (you'll need the Admin app enabled).
+
+5. Visit http://127.0.0.1:8000/survey/<survey_id> to fill in survey
+6. Visit http://127.0.0.1:8000/survey/<survey_id>/download to download a csv of submissions
--- setup.py
+++ setup.py
@@ -1,81 +1,37 @@
-"""A setuptools based setup module.
+import os
+from setuptools import find_packages, setup
 
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='forman',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
-
-    description='form builder for django',
-    long_description=long_description,
-
-    # The project's main homepage.
-    url='https://github.com/AlSayedGamal/forman',
-
-    # Author details
+    version='0.1.2',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='A simple Django app to create surveys and manage submissions',
+    long_description=README,
+    url='https://www.cognitev.com/',
     author='Cognitev',
     author_email='ag@menacommerce.com',
-
-    # Choose your license
-    license='MIT',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',  # replace "X.Y" as appropriate
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
+        'License :: OSI Approved :: BSD License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
-
-    # What does your project relate to?
-    keywords='Form builder google-forms',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['django'],
 )
